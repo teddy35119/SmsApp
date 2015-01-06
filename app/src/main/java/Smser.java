d8f8b8c43835f7857861eb234727682a1f1be17b
teddy35119 <teddy35119@gmail.com>
2015-01-06 20:56:27 +0800

    Initial commit

---
+++ app/src/main/java/Smser.java
@@ -0,0 +1,37 @@
+import java.util.Calendar;
+
+/**
+ * Created by teddy on 2014/12/31.
+ */
+public class Smser {
+    private Calendar SmsInTime;
+    private Calendar SmsOutTime;
+    private int DiscountDay;
+    private int SmsYear;
+    private int SmsDay;
+    private int LeaveDay;
+
+    public void Smser()
+    {
+
+    }
+    public void setSmsInTime(Calendar userSmsInTime){
+        SmsInTime = userSmsInTime;
+    }
+    public Calendar getSmsInTime(){
+        return SmsInTime;
+    }
+    public Calendar getSmsOutTime(){
+        SmsOutTime.add(Calendar.DAY_OF_YEAR,SmsYear);
+        SmsOutTime.add(Calendar.DAY_OF_YEAR,SmsDay);
+        return SmsOutTime;
+    }
+    public void setSmsLifeDay(int userYear,int userDay){
+        if(userYear >= 0 && userDay >= 0)
+        {
+            SmsYear = userYear;
+            SmsDay = userDay;
+        }
+    }
+
+}
