68254922d7887895d7e2ea449eb3c9cce868b6ff
teddy35119 <teddy35119@gmail.com>
2015-08-19 23:12:21 +0800

    1.add ShareActionProvider 2.Can share text

--- app/src/main/java/com/view/MainActivity.java
+++ app/src/main/java/com/view/MainActivity.java
@@ -1,15 +1,20 @@
 package com.view;
 
 
+import android.app.Activity;
 import android.content.Intent;
-import android.content.SharedPreferences;
+
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import android.widget.ProgressBar;
+import android.support.v7.widget.ShareActionProvider;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,7 +22,6 @@ import com.example.teddy.smsapp.R;
 import com.sms.Preference;
 import com.sms.Smser;
 
-import java.text.NumberFormat;
 import java.util.Calendar;
 
 
@@ -30,23 +34,48 @@ public class MainActivity extends ActionBarActivity {
         showInitCompoment();
         SmsPreference = new Preference(MainActivity.this);
         mainWork();
-        Toast.makeText(this,"OnCreate3",Toast.LENGTH_SHORT).show();
+        Toast.makeText(this,"OnCreate5",Toast.LENGTH_SHORT).show();
 
     }
 
-
+    private ShareActionProvider mShareActionProvider;
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        // Set up ShareActionProvider's default share intent
+        MenuItem shareItem = menu.findItem(R.id.menu_item_share);
+
+        mShareActionProvider =(ShareActionProvider) MenuItemCompat.getActionProvider(shareItem);
+
+        setShareIntent(SetIntent());
         return true;
     }
+
+    private Intent SetIntent(){
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, "我剩下" + LeaveDay +"天");
+        sendIntent.setType("text/plain");
+        return sendIntent;
+    }
+
+    private void setShareIntent(Intent shareIntent) {
+        if (mShareActionProvider != null) {
+            mShareActionProvider.setShareIntent(shareIntent);
+        }else{
+            Log.v("test","isNull");
+        }
+    }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
+
         int id = item.getItemId();
         Intent SettingIntent = new Intent();
         switch (id){
@@ -58,6 +87,15 @@ public class MainActivity extends ActionBarActivity {
             case R.id.action_widget :
                 SettingIntent.setClass(MainActivity.this,WidgetSettingActivity.class);
                 startActivity(SettingIntent);
+                break;
+            case R.id.menu_item_share :
+                /*Toast.makeText(this,"InShare",Toast.LENGTH_SHORT).show();
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_TEXT, "我剩下"+LeaveDay);
+                sendIntent.setType("text/plain");
+                sendIntent.setPackage("jp.naver.line.android");
+                startActivity(sendIntent);*/
                 break;
         }
         return super.onOptionsItemSelected(item);
@@ -86,12 +124,14 @@ public class MainActivity extends ActionBarActivity {
         SmsPorgressBar = (ProgressBar)findViewById(R.id.progressBar);
         RemainPercentText = (TextView)findViewById(R.id.RemainPercentText);
     }
+    private int LeaveDay;
     public void mainWork(){
 
         ShowSms = SmsPreference.showWork() ;
         SmsInText.setText(getText(R.string.SmsInDate) + ShowSms.dateFormat(ShowSms.getSmsYear(), ShowSms.getSmsMonth(), ShowSms.getSmsDay()));
 
         ShowSmsTIME();
+        LeaveDay = ShowSms.getLeaveDay();
         LeaveDayText.setText(getText(R.string.SmsOutDate) + String.valueOf(ShowSms.getLeaveDay()));
 
         //取出當兵的總天數
